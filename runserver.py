60d19e4e7adb72fd93f0644d2b939077a38933cb
Brian McMichael <brian@brianmcmichael.com>
2016-07-23 16:42:07 -0400

    add debugmode to Flask server

--- runserver.py
+++ runserver.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#Settings
+DEBUG=True
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=DEBUG)
